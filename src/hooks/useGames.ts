9adadef8613969100bed5e6ecd0f9501e76f6daa
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2023-06-29 16:26:55 -0400

    Storing Platform Information

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,12 +2,20 @@ import { useEffect, useState } from "react";
 import apiToClient from "../services/apiToClient";
 
 
+//whole interface for each game in the api
 export interface Games {
-    id: number;
-    name: string;
-    background_image: string;
+    id: number; //ID for each Game
+    name: string; // Name of each Game
+    background_image: string; // The image associated with each Game
+    parent_platforms: {platform : Platform}[] //cant be an array of objects because of how the api was set up. has to be an object with platform property
   }
   
+  //Inteface for the Platform Object, Will contain the information for the platform for the games
+  interface Platform{
+    id : number;
+    name: string;
+    slug: string;
+  }
   interface FetchedGames {
     count: number;
     results: Games[];
